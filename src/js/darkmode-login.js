8fc30957b40490ecc37133c56000449c199f8643
wytreep <edwinacarabali@gmail.com>
2026-02-24 21:03:34 -0500

    fix rediseño de estilo

--- src/js/darkmode-login.js
+++ src/js/darkmode-login.js
@@ -1,13 +1,15 @@
 const btnDarkModeLogin = document.getElementById("btnDarkModeLogin")
 
 if (localStorage.getItem("darkmode-login") === "true") {
-    document.body.classList.add("dark")
+    document.body.classList.add("light")
+    btnDarkModeLogin.textContent = "🌙"
+} else {
     btnDarkModeLogin.textContent = "☀️"
 }
 
 btnDarkModeLogin.addEventListener("click", function() {
-    document.body.classList.toggle("dark")
-    const isDark = document.body.classList.contains("dark")
-    localStorage.setItem("darkmode-login", isDark)
-    btnDarkModeLogin.textContent = isDark ? "☀️" : "🌙"
+    document.body.classList.toggle("light")
+    const isLight = document.body.classList.contains("light")
+    localStorage.setItem("darkmode-login", isLight)
+    btnDarkModeLogin.textContent = isLight ? "🌙" : "☀️"
 })
